f8c744555f0d7214f02e87efd7f0116090c9d1e1
Rizwan Nargundkar <UmarShaikMNargundkar.Rizwan@in.bosch.com>
2021-06-17 16:49:48 +0530

    Limit complexity in Receiver.cs

--- src/Receiver/Receiver/Receiver.cs
+++ src/Receiver/Receiver/Receiver.cs
@@ -9,6 +9,7 @@ namespace Receiver
         {
             string readParameter;
             List<string> inputParameters = new List<string>();
+            BatteryParameters parameters;
             int i = 1;
             while ((readParameter = Console.ReadLine()) != null)
             {
@@ -19,22 +20,28 @@ namespace Receiver
                     var parameterParser = new ParameterParser(new JSONParameterParser());
                     List<BatteryParameters> batteryParameters = parameterParser.Parser(inputParameters);
                     ParameterStatistics parameterStatistics = new ParameterStatistics();
-                    BatteryParameters parameters;
+                    
                     parameters = parameterStatistics.GetMaximumValues(batteryParameters);
                     Console.WriteLine("Maximum of soc " + parameters.StateOfCharge + " and maximum of temperature " + parameters.Temperature);
-                    
+
                     parameters = parameterStatistics.GetMinimumValues(batteryParameters);
                     Console.WriteLine("Minimum of soc " + parameters.StateOfCharge + " and minimum of temperature " + parameters.Temperature);
-
-                    if (batteryParameters.Count > 5)
-                    {
-                        parameters = parameterStatistics.GetAverageOfParameters(batteryParameters.GetRange(batteryParameters.Count - 5, 5));
-                        Console.WriteLine("Average of soc " + parameters.StateOfCharge + " and Average of temperature " + parameters.Temperature);
-                    }
+                    PrintAverage(batteryParameters, parameterStatistics, parameters);
                     i = 1;
                     inputParameters.Clear();
                 }
             }
+        }
+
+        private static BatteryParameters PrintAverage(List<BatteryParameters> batteryParameters, ParameterStatistics parameterStatistics, BatteryParameters parameters)
+        {
+            if (batteryParameters.Count > 5)
+            {
+                parameters = parameterStatistics.GetAverageOfParameters(batteryParameters.GetRange(batteryParameters.Count - 5, 5));
+                Console.WriteLine("Average of soc " + parameters.StateOfCharge + " and Average of temperature " + parameters.Temperature);
+            }
+
+            return parameters;
         }
     }
 }
