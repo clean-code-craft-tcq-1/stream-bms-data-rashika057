15c1eed80631bd1299f61abaec2b40fdb3e8153f
rashika057 <rashika059@gmail.com>
2021-06-21 17:13:06 +0530

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,5 +1,5 @@
 pushd "%~dp0"
 
-java -ea -classpath src\ sender.BmsDataStream | dotnet run --project src\Receiver\Receiver\Receiver.csproj
+java -ea -classpath -classpath ".:.\lib\*" sender.BmsDataStream | dotnet run --project src\Receiver\Receiver\Receiver.csproj
 
 popd
