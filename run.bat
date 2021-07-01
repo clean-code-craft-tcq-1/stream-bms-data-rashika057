747432b9acaf2a66698582809aa69257844df0af
Rashika Shukla <Rashika.Shukla@in.bosch.com>
2021-07-01 10:36:32 +0530

    changed the structuring of folder

--- run.bat
+++ run.bat
@@ -1,5 +1,5 @@
 pushd "%~dp0"
 
-java -ea -cp "src;lib\*" sender.BmsDataStream 25 | dotnet run --project src\Receiver\Receiver\Receiver.csproj
+java -ea -cp "src;lib\*" main.java.sender.BmsDataStream 25 | dotnet run --project src\Receiver\Receiver\Receiver.csproj
 
 popd
--- src/main/java/sender/BmsDataStream.java
+++ src/main/java/sender/BmsDataStream.java
@@ -1,4 +1,4 @@
-package sender;
+package main.java.sender;
 
 import java.io.FileReader;
 import java.io.IOException;
